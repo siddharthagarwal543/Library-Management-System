d118e6f310cd21a85615e2863ccf9fccc53d1ffd
siddharthagarwal543 <siddharthagarwal543@gmail.com>
2023-11-17 09:45:54 +0530

    Added Class concpets

--- app.py
+++ app.py
@@ -1,96 +1,113 @@
 import re
 
+class libraryManager:
+    def __init__(self):
+        self.database = self.read_database_from_file()
 
+    def read_database_from_file(self):
+        database = []
+        try:
+            with open("database.txt", "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    record = line.strip().split(",")
+                    book_info = {
+                        'author': record[0],
+                        'title': record[1],
+                        'quantity': int(record[2]),
+                        'status': record[3]
+                    }
+                    print(book_info)
+                    database.append(book_info)
+            return database
+        except FileNotFoundError:
+            return []
 
-# database = []
+    def write_database_to_file(self):
+        with open("database.txt", "w") as file:
+            for record in self.database:
+                res=""
+                for key in record:
+                    res+=str(record[key])
+                    if key=='status':
+                        res+="\n"
+                    else:
+                        res+=","
+                file.write(res)
 
-def read_database_from_file():
-    database = []
-    try:
-        with open("database.txt", "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                record = line.strip().split(",")
-                book_info = {
-                    'author': record[0],
-                    'title': record[1],
-                    'quantity': int(record[2]),
-                    'status': record[3]
-                }
-                database.append(book_info)
-        return database
-    except FileNotFoundError:
-        return []
+    def search(self, option_1, option_2):
+        found_records = []
+        if option_1 == 1:
+            pattern = re.compile('^' + option_2, re.IGNORECASE) 
+            found_records = [record for record in self.database if pattern.match(record['author'])]
+        else:
+            pattern = re.compile('^' + option_2, re.IGNORECASE)
+            found_records = [record for record in self.database if pattern.match(record['title'])]
 
-def write_database_to_file(database):
-    with open("book_database.txt", "w") as file:
-        for record in database:
-            file.write(','.join(record) + '\n')
-    return
+        for record in found_records:
+            print(record['title'], " ", record['author'], " ", record['quantity'], " ", record['status'])
 
-def search(option_1, option_2):
-    found_records = []
-    if option_1 == 1:
-        pattern = re.compile('^' + option_2, re.IGNORECASE) 
-        found_records = [record for record in database if pattern.match(record['author'])]
-    else:
-        pattern = re.compile('^' + option_2, re.IGNORECASE)
-        found_records = [record for record in database if pattern.match(record['title'])]
-
-    for record in found_records:
-        print(record['title'], " ", record['author'], " ", record['quantity'], " ", record['status'])
-
-def issue(author, title):
-    pattern1=re.compile('^'+author,re.IGNORECASE)
-    pattern2=re.compile('^'+title,re.IGNORECASE)
-    for record in database:
-        if pattern1.match(record['author']) and pattern2.match(record['title']):
-            if record['quantity'] == 0:
-                print("No books available!")
+    def add_new_book(self, author, title, quantity):
+        for record in self.database:
+            if record['author'] == author and record['title'] == title:
+                record['quantity']+=quantity
+                print("Book already there so qunatity updated!")
                 return
+        self.database.append({'author': author, 'title': title, 'quantity': quantity, 'status': 'available'})
+        print("New Book added successfully!")
 
-            record['quantity'] -= 1
-            if record['quantity'] == 0:
-                record['status'] = 'Not Available'
-            print("Book issued successfully!")
-    return
+    def issue(self, author, title):
+        pattern1 = re.compile('^'+author, re.IGNORECASE)
+        pattern2 = re.compile('^'+title, re.IGNORECASE)
+        for record in self.database:
+            if pattern1.match(record['author']) and pattern2.match(record['title']):
+                if record['quantity'] == 0:
+                    print("No books available!")
+                    return
+                record['quantity'] -= 1
+                if record['quantity'] == 0:
+                    record['status'] = 'Not Available'
+                print("Book issued successfully!")
+                return
+        print("Book not found in the database!")
 
-    print("Book not found in the database!")
+    def book_return(self, author, title):
+        for record in self.database:
+            if record['author'].upper() == author.upper() and record['title'].upper() == title.upper():
+                record['quantity'] += 1
+                if record['quantity'] == 1:
+                    record['status'] = 'Available'
+                print('Book successfully returned!')
+                return
+        print("Enter valid details!")
 
-def book_return(author, title):
-    for record in database:
-        if record['author'].upper() == author.upper() and record['title'].upper() == title.upper():
-            record['quantity'] += 1
-            if record['quantity'] == 1:
-                record['status'] = 'Available'
-            print('Book successfully returned!')
-            return
-    print("Enter valid details!")
-    return
+library_manager = libraryManager()
 
-database = read_database_from_file() 
-choice=1
-while choice==1 or choice==2:
-    choice = int(input("Enter:\n1 for Search and Issue\n2 for Return\n Any other number for exiting\n"))
-
-    if choice == 1:
+choice = 1
+while choice in (0, 1, 2):
+    choice = int(input("Enter:\n0 for Adding New Book \n1 for Search and Issue\n2 for Return\n Any other number for exiting\n"))
+    if choice == 0:
+        author = input('Enter author name: ')
+        title = input('Enter book title: ')
+        quantity = int(input('Enter quantity: '))
+        library_manager.add_new_book(author, title, quantity)
+    elif choice == 1:
         option_2 = int(input("Enter:\n1 for Author\n2 for Title\n"))
         if option_2 == 1:
             author = input("Enter author name: ")
-            search(option_2, author)
+            library_manager.search(option_2, author)
             title = input("Enter book title you want to issue: ")
-            issue(author, title)
-
+            library_manager.issue(author, title)
         elif option_2 == 2:
             title = input("Enter Book Title: ")
-            search(option_2, title)
+            library_manager.search(option_2, title)
             author = input("Enter author name you want to issue: ")
-            issue(author, title)
-
+            library_manager.issue(author, title)
     elif choice == 2:
         author = input("Enter author name: ")
         title = input("Enter Book title: ")
-        book_return(author, title)
+        library_manager.book_return(author, title)
     else:
         break
-write_database_to_file(database)
+
+library_manager.write_database_to_file()
